b38d00f49968379be38f6b73fd7b0de60c3a7431
duke-doki <145125297+duke-doki@users.noreply.github.com>
2024-08-13 14:20:24 +0300

    rm -w

--- render.sh
+++ render.sh
@@ -3,11 +3,14 @@
 VERSION="main"
 BASE_IMAGE="ubuntu"
 CHECKSUM="9adccb8fe17a40252df1a3acdea7edef4633b4ecaa8ba2dd5e0270f87ae43eab"
+INCLUDE_W_FLAG="false"
+
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -version) VERSION="$2"; shift ;;
         -base-image) BASE_IMAGE="$2"; shift ;;
+        -w) INCLUDE_W_FLAG="true" ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
@@ -21,7 +24,6 @@ else
     TAG="${VERSION}-${BASE_IMAGE}"
     DIR1="0.1"
 fi
-
 
 if [ "$BASE_IMAGE" == "ubuntu" ]; then
     DIR2="ubuntu"
@@ -44,8 +46,10 @@ fi
 
 docker build --build-arg VERSION="${VERSION}" --build-arg CHECKSUM="${CHECKSUM}" -t "static-jinja-plus:${TAG}" -f "${DIR1}/${DIR2}/Dockerfile" .
 
+
 docker run -d --name static-jinja-plus_container_temp \
     "static-jinja-plus:${TAG}"
+
 
 if [ ! -d "$(pwd)/templates" ] || [ -z "$(ls -A "$(pwd)/templates")" ]; then
     docker cp static-jinja-plus_container_temp:/opt/StaticJinjaPlus/templates "$(pwd)/templates"
@@ -55,9 +59,18 @@ if [ ! -d "$(pwd)/build" ] || [ -z "$(ls -A "$(pwd)/build")" ]; then
     docker cp static-jinja-plus_container_temp:/opt/StaticJinjaPlus/build "$(pwd)/build"
 fi
 
+
 docker rm -f static-jinja-plus_container_temp
 
-docker run -d --name static-jinja-plus_container \
-    -v "$(pwd)/templates:/opt/StaticJinjaPlus/templates" \
-    -v "$(pwd)/build:/opt/StaticJinjaPlus/build" \
-    "static-jinja-plus:${TAG}"
+
+if [ "$INCLUDE_W_FLAG" == "true" ]; then
+    docker run -d --name static-jinja-plus_container \
+        -v "$(pwd)/templates:/opt/StaticJinjaPlus/templates" \
+        -v "$(pwd)/build:/opt/StaticJinjaPlus/build" \
+        "static-jinja-plus:${TAG}" -w
+else
+    docker run -d --name static-jinja-plus_container \
+        -v "$(pwd)/templates:/opt/StaticJinjaPlus/templates" \
+        -v "$(pwd)/build:/opt/StaticJinjaPlus/build" \
+        "static-jinja-plus:${TAG}"
+fi
